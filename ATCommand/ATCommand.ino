d9c5a3d54e5718e7df1a9f3d27e348204b67b1af
HJK-Z <hjk.businessemail@gmail.com>
2020-12-31 02:29:50 -0800

    update

--- ATCommand/ATCommand.ino
+++ ATCommand/ATCommand.ino
@@ -1,12 +1,12 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BTSerial(10, 11); // RX | TX 
-// Connect the HC-05 TX to Arduino pin 10 RX.
-// Connect the HC-05 RX to Arduino pin 11 TX
+// Connect the HC-05 TX to Arduino pin 10
+// Connect the HC-05 RX to Arduino pin 11
 
 
 void setup() {
-  pinMode(9, OUTPUT);
+  pinMode(9, OUTPUT);       // Connect HC-05 Key to Arduino pin 9
   digitalWrite(9, HIGH);
   Serial.begin(9600);
   Serial.println("NL & CR + 9600 BAUD");
